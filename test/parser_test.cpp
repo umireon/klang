11786aabd95816b00787d87bc97731d9704000b9
Udagawa Kaito <umireon@gmail.com>
2014-05-14 21:03:33 +0900

    10進，8進，16進正数および0のパースを実装した．Build Passing．

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -23,16 +23,28 @@ TEST(Parser, ParseDec)
     CHECK_EQUAL(AST_NUMBER, stmt->type);
     CHECK_EQUAL(0, stmt->num_of_child);
     CHECK_EQUAL(1, stmt->value);
+
+    stmt = parse_number("11");
+    CHECK_EQUAL(11, stmt->value);
+
+    stmt = parse_number("121");
+    CHECK_EQUAL(121, stmt->value);
 }
 
 
 TEST(Parser, ParseHex)
 {
     struct ast_node *stmt;
-    stmt = parse_number("0x2");
+    stmt = parse_number("0xA");
     CHECK_EQUAL(AST_NUMBER, stmt->type);
     CHECK_EQUAL(0, stmt->num_of_child);
-    CHECK_EQUAL(2, stmt->value);
+    CHECK_EQUAL(0xA, stmt->value);
+
+    stmt = parse_number("0xF5");
+    CHECK_EQUAL(0xF5, stmt->value);
+
+    stmt = parse_number("0Xf5");
+    CHECK_EQUAL(0xF5, stmt->value);
 }
 
 TEST(Parser, ParseOct)
@@ -41,5 +53,11 @@ TEST(Parser, ParseOct)
     stmt = parse_number("03");
     CHECK_EQUAL(AST_NUMBER, stmt->type);
     CHECK_EQUAL(0, stmt->num_of_child);
-    CHECK_EQUAL(2, stmt->value);
+    CHECK_EQUAL(3, stmt->value);
+
+    stmt = parse_number("010");
+    CHECK_EQUAL(010, stmt->value);
+
+    stmt = parse_number("0");
+    CHECK_EQUAL(0, stmt->value);
 }
