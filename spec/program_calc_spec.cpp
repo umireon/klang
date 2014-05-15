1d9b5eee1c8af82798168ad19220b892e2c76a2d
Udagawa Kaito <umireon@gmail.com>
2014-05-15 22:49:44 +0900

    テストごとにPendingを指定した．

--- spec/program_calc_spec.cpp
+++ spec/program_calc_spec.cpp
@@ -1,6 +1,8 @@
 #include <igloo/igloo_alt.h>
 using namespace igloo;
 
+#define Pending printf("P");return
+
 bool accepts(const char *str)
 {
 	printf("P");
@@ -8,31 +10,31 @@ bool accepts(const char *str)
 }
 
 Describe(プログラム電卓) {
-	It(四則演算を受け入れる) {
+	It(四則演算を受け入れる) {Pending;
 		Assert::That(accepts("1+2*3-4/5"));
 	}
-	It(丸括弧を受け入れる) {
+	It(丸括弧を受け入れる) {Pending;
 		Assert::That(accepts("2*(3+4)"));
 	}
-	It(実数を受け入れる) {
+	It(実数を受け入れる) {Pending;
 		Assert::That(accepts("1.234"));
 	}
-	It(べき乗を受け入れる) {
+	It(べき乗を受け入れる) {Pending;
 		Assert::That(accepts("2**3"));
 	}
-	It(剰余を受け入れる) {
+	It(剰余を受け入れる) {Pending;
 		Assert::That(accepts("4%2"));
 	}
-	It(三角関数を受け入れる) {
+	It(三角関数を受け入れる) {Pending;
 		Assert::That(accepts("sin(1)"));
 	}
-	It(対数関数を受け入れる) {
+	It(対数関数を受け入れる) {Pending;
 		Assert::That(accepts("log(1)"));
 	}
-	It(代入を受け入れる) {
+	It(代入を受け入れる) {Pending;
 		Assert::That(accepts("a=2"));
 	}
-	It(比較演算子を受け入れる) {
+	It(比較演算子を受け入れる) {Pending;
 		Assert::That(accepts("1 <= 2"));
 		Assert::That(accepts("1 < 2"));
 		Assert::That(accepts("2 > 1"));
@@ -40,11 +42,11 @@ Describe(プログラム電卓) {
 		Assert::That(accepts("1 == 1"));
 		Assert::That(accepts("1 != 2"));
 	}
-	It(if文を受け入れる) {
+	It(if文を受け入れる) {Pending;
 		Assert::That(accepts("if 1 < 2 then 2 elsif 3 <= 4 then 4 else 5 end"));
 		Assert::That(accepts("if (1 < 2) {2} elsif (3 <= 4) {4} else {5}"));
 	}
-	It(三項演算子を受け入れる) {
+	It(三項演算子を受け入れる) {Pending;
 		Assert::That(accepts("1<2?2:3"));
 	}
 };
