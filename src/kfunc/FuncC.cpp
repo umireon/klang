208ed9b7179ab73d6a3d4c81e21f73d6515b6283
Kaito Udagawa <umireon@gmail.com>
2014-06-04 23:05:21 +0900

    C関数はベクトルをつぶす．

--- src/kfunc/FuncC.cpp
+++ src/kfunc/FuncC.cpp
@@ -1,18 +1,40 @@
 #include "kobject.h"
 
+#include <vector>
+
 #include "kfunc/FuncC.h"
 
 KObject* FuncC::invoke(std::vector<KObject*> args) {
-    dvector vect(args.size());
+    std::vector<KObject *>::iterator iter = args.begin();
+    std::vector<double> list;
 
-    for (int i = 0; i < args.size(); i++) {
-        KNumber *knum = dynamic_cast<KNumber*>(args[i]);
-        if (knum == NULL) {
-            throw std::invalid_argument(std::string("not a number"));
+    while (iter != args.end()) {
+        switch ((*iter)->get_type()) {
+            case KObject::INTEGER:
+            case KObject::FLOAT:
+                {
+                    KNumber *knum = static_cast<KNumber *>(*iter);
+                    list.push_back(knum->to_f());
+                }
+                break;
+            case KObject::VECTOR:
+                {
+                    KVector *kvect = static_cast<KVector *>(*iter);
+                    dvector &vect = kvect->vect;
+                    list.insert(list.end(), vect.begin(), vect.end());
+                }
+                break;
+            default:
+                ;
         }
-        vect[i] = knum->to_f();
-        delete knum;
+
+        delete *iter;
+        iter++;
     }
 
+    dvector vect(list.size());
+    for (int i = 0; i < list.size(); i++) {
+        vect[i] = list[i];
+    }
 	return new KVector(vect);
 }
