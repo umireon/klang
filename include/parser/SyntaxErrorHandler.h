9a8038dd0d81ede271b2f6047c43a9f1f6b45f1b
Kaito Udagawa <umireon@gmail.com>
2014-06-05 23:21:42 +0900

    SEHの入出力を指定可能にした．

--- include/parser/SyntaxErrorHandler.h
+++ include/parser/SyntaxErrorHandler.h
@@ -4,8 +4,12 @@
 
 class SyntaxErrorHandler {
 public:
+	std::ostream &output;
 	std::string *line;
 	int lineno;
+
+	SyntaxErrorHandler() : output(std::cout) {}
+	SyntaxErrorHandler(std::ostream &os) : output(os) {}
 
 	pstr_t invalid_char(pstr_t str, const char *func);
 	pstr_t never_reach(pstr_t str, const char *func);
