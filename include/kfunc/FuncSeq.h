7f1d8b84dea775441b42754978095bd9bdf1aecf
Kaito Udagawa <umireon@gmail.com>
2014-06-05 23:41:44 +0900

    FuncSeqのテストを詳細か

--- include/kfunc/FuncSeq.h
+++ include/kfunc/FuncSeq.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 #include "kobject/KObject.h"
@@ -7,7 +8,13 @@
 
 class FuncSeq : public KFunction {
 public:
+    FuncSeq() : outerror(std::cerr) {}
+    FuncSeq(std::ostream &os) : outerror(os) {}
+
     KObject *invoke(std::vector<KObject*> args);
     KVector *invoke(KNumber *first, KNumber *last);
 	virtual FuncSeq *clone() const { return new FuncSeq(*this); }
+
+private:
+    std::ostream &outerror;
 };
