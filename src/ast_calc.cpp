b86017d6008d8ff43dfb648b706928774549964b
Kaito Udagawa <umireon@gmail.com>
2014-06-05 18:15:40 +0900

    ルールを統一．

--- src/ast_calc.cpp
+++ src/ast_calc.cpp
@@ -124,13 +124,13 @@ void make_world(Binding *b)
     b->set_local(std::string("G"), new KFloat(6.67384*pow(10,-11)));        //banyuuinryokuteisuu
     b->set_local(std::string("H"), new KFloat(6.62606957*pow(10,-34)));     //Planck's consntan
     b->set_local(std::string("K"), new KFloat(1.3806488*pow(10,-23)));      //Boltzmann constant
-    b->set_local(std::string("Rydbergconst"), new KFloat(10973731.568539));    //Rydberg consntant
+    b->set_local(std::string("RYDBERG_CONST"), new KFloat(10973731.568539));    //Rydberg consntant
     b->set_local(std::string("E0"), new KFloat(8.85418782*pow(10,-12)));   //e0
-    b->set_local(std::string("Mu0"), new KFloat(1.25663706*pow(10,-6)));   //mu0
-    b->set_local(std::string("Ec"), new KFloat(1.60217657*pow(10,-19)));   //elementary charge
-    b->set_local(std::string("Mach"), new KFloat(340.29));  //speed of sound
+    b->set_local(std::string("MU0"), new KFloat(1.25663706*pow(10,-6)));   //mu0
+    b->set_local(std::string("EC"), new KFloat(1.60217657*pow(10,-19)));   //elementary charge
+    b->set_local(std::string("MACH"), new KFloat(340.29));  //speed of sound
 
-    b->set_local(std::string("GravityEarth"), new KFloat(9.80665));    //gravity on earth
+    b->set_local(std::string("GRAVITY_EARTH"), new KFloat(9.80665));    //gravity on earth
 }
 
 std::string create_prompt(int lineno, int depth)
